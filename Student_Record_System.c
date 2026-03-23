791dcf9728e5066027513186c787a748f00a647e
johnpaul <moikoyojohnpaul@gmail.com>
2026-03-23 11:41:01 +0300

    Feat: Implemented displayRecord function

--- Student_Record_System.c
+++ Student_Record_System.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<string.h>
 typedef struct{
-    char iD;
+    char iD[20];
     char name[50];
     float gpa;
 }Student;
@@ -15,7 +15,7 @@ int main()
     int choice;
     while(1)
     {
-        printf("---STUDENT RECORD SYSTEM---");
+        printf("\n---STUDENT RECORD SYSTEM---");
         printf("\n1. Add record \n2. Search record \n3. Delete record \n4. Display records \n5. Exit\n");
         printf("Select your choice,(1,2,3,4,5): ");
         scanf(" %d", &choice);
@@ -46,7 +46,7 @@ void addRecord()
         return;
     }
     printf("Enter student's ID: ");
-    scanf("%s", &s.iD);
+    scanf("%s", s.iD);
     getchar();
     printf("Enter student's name: ");
     fgets(s.name, sizeof(s.name), stdin);
@@ -59,13 +59,27 @@ void addRecord()
 }
 void searchRecord()
 {
-    printf("Feature coming up soon...");
+    printf("Feature coming up soon...\n");
 }
 void deleteRecord()
 {
-    printf("Feature coming up soon...");
+    printf("Feature coming up soon...\n");
 }
 void displayRecords()
 {
-    printf("Feature coming up soon...");
+    Student s;
+    FILE *studrecordsfile = fopen("studrecords.dat", "rb");
+    if (studrecordsfile == NULL)
+    {
+        printf("Error opening file! No records yet.");
+        return;
+    }
+    printf("<< STUDENT RECORD SYSTEM DISPLAY >>")
+    printf("\n%-10s %-20s %-5s", "ID", "Name", "Gpa");
+    printf("\n");
+    while(fread(&s, sizeof(Student), 1, studrecordsfile) == 1)
+    {
+        printf("%-10s %-20s %.2f\n", s.iD, s.name, s.gpa);
+    }
+    fclose(studrecordsfile);
 }
